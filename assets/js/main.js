18b342de94acbe9e28d5afd6d709a8d4141ae1ba
Sidemar Schimmelpfennig Junior <90000000+sidemarschimmelpfennig@users.noreply.github.com>
2023-03-01 06:36:16 -0300

    json data request

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,34 +3,23 @@ const profileInformation = (profileData) =>{
     //photo perfil
     const photo =  document.getElementById('profile-photo')
     photo.src   = profileData.photo
-
     //name
     const name = document.getElementById('profile-name')
     name.innerHTML = profileData.name
-
     //job
     const job = document.getElementById('profile-job')
     job.innerHTML = profileData.job
-
     //location
-
     const location = document.getElementById('profile-location')
-
-
+    location.innerHTML = profileData.location
+    //phone
     const phone = document.getElementById('profile-phone')
     phone.innerHTML = profileData.phone
-
+    //email
     const email = document.getElementById('profile-email')
-    email.innerHTML = profileData.email[name]
-    email.href = `mailto:${profileData.email}`
+    email.innerHTML = profileData.email.name
+    email.href = profileData.email.href
 }
-
-
-
-
-
-
-
 
 (async () => {
         const fetchProfile = await fetchProfileData()
