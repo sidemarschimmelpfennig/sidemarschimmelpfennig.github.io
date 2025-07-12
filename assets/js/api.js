a2aee3aaa34c143dc57dd621db69b36f3cf4bc51
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2025-07-12 00:57:42 -0300

    alter api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
  
 async function fetchProfileData (){
-    const urlMyGitData = "https://raw.githubusercontent.com/sidemarschimmelpfennig/sidemarschimmelpfennig.github.io/dio/data/profile.json"
+    const urlMyGitData = "https://raw.githubusercontent.com/sidemarschimmelpfennig/sidemarschimmelpfennig.github.io/refs/heads/main/data/profile.json"
     const fetching = await fetch (urlMyGitData) 
     return await fetching.json()
 }
